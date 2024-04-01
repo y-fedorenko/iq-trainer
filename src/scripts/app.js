e832a5d85759b6f2202c1dc7d44b4110f6bd4cf2
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-04-01 17:33:09 -0500

    added soundeffects

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,7 +9,16 @@ const input = document.querySelector('.input-box');
 const currentWord = document.querySelector('h2');
 const timer = document.querySelector('h1');
 
-let time = 99.0;
+const backgroundSound = new Audio('./src/media/backgroundsound.mp3');
+backgroundSound.type = 'audio/mpeg';
+backgroundSound.loop = true;
+backgroundSound.volume = 0.5;
+const gameEndSound = new Audio('./src/media/gameendsound.mp3');
+gameEndSound.type = 'audio/mpeg';
+gameEndSound.volume = 1.0;
+
+
+let time = 15.0;
 let hitsCount = 0;
 let words = [...wordsList].sort(() => Math.random() - 0.5);
 let index = words.length - 1;
@@ -18,10 +27,24 @@ function startTimer() {
   timer.innerText = time.toFixed(0);
   time -= 1; 
   if (time < 0) {
-    /* reset here */
+    gameEndSound.play();
+    backgroundSound.pause();
     return;
   }
   setTimeout(startTimer, 1000);
+}
+
+//The sounds are in separate functions so they can be played from start each time
+function playSoundCorrect() {
+  const correctSound = new Audio('./src/media/correctsound.mp3');
+  correctSound.type = 'audio/mpeg';
+  correctSound.loop = false;
+  correctSound.play();
+}
+
+function playSoundBackground() {
+  backgroundSound.currentTime = 0;
+  backgroundSound.play();
 }
 
 function playWords() {
@@ -33,7 +56,7 @@ function playWords() {
       hitsCount += 1;
       index -= 1;
       input.value = '';
-      /* some music here */
+      playSoundCorrect();
       input.removeEventListener('input', checkInput);
       playWords();
     }
@@ -41,10 +64,11 @@ function playWords() {
 
   input.addEventListener('input', checkInput);
 }
-function hideStartButton() {
+function StartButtonClicked() {
   startButton.classList.add('hidden');
   playWords();
+  playSoundBackground();
 }
 
-startButton.addEventListener('click', hideStartButton);
+startButton.addEventListener('click', StartButtonClicked);
 startButton.addEventListener('click', startTimer);
