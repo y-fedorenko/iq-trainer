14b8fb57d3d1b47b16a6425bbca8e868c8ce94c5
Yaroslav Fedorenko <y.fedorenko@gmail.com>
2024-04-01 15:34:53 -0500

    basic html, css and js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const screen = document.querySelector('.screen');
+const startButton = document.querySelector('#start');
+const input = document.querySelector('.input-box');
+const hitsCounter = document.querySelector('h2');
+const timer = document.querySelector('h1');
+
+let time = 99.0;
+
+function startTimer() {
+  timer.innerText = time.toFixed(1);
+  time -= 0.1; 
+  if (time < 0) return;
+  setTimeout(startTimer, 100);
+}
+
+
+function hideStartButton() {
+  startButton.classList.add('hidden');
+}
+
+startButton.addEventListener('click', hideStartButton);
+startButton.addEventListener('click', startTimer);
